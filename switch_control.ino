f581a5ce87059be115f8a8878d556d6ca0442d79
annaemorgan <112512488+annaemorgan@users.noreply.github.com>
2024-10-10 11:46:26 -0700

    Create switch_control.ino

---
+++ switch_control.ino
@@ -0,0 +1,11 @@
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("IDN ?");  // This sends 'IDN ?<CR><LF>'
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
